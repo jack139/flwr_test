3de732310f8ac100fc5d2195c4e79f5070881e7c
gt <jack139@gmail.com>
2022-09-29 16:11:31 +0800

    update

--- advanced_pytorch2/run.sh
+++ advanced_pytorch2/run.sh
@@ -5,8 +5,8 @@ cd "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/
 # Download the CIFAR-10 dataset
 python3 -c "from torchvision.datasets import CIFAR10; CIFAR10('./data', download=True)"
 
-#python3 server.py &
-#sleep 3  # Sleep for 3s to give the server enough time to start
+python3 server.py &
+sleep 3  # Sleep for 3s to give the server enough time to start
 
 for i in `seq 0 9`; do
     echo "Starting client $i"
