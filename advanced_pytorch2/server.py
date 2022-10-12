bfed538799be474b46ca7102553b43108ab5b282
gt <jack139@gmail.com>
2022-10-12 17:33:33 +0800

    customerized strategy

--- advanced_pytorch2/server.py
+++ advanced_pytorch2/server.py
@@ -1,10 +1,14 @@
-from typing import Dict, Optional, Tuple
+import os
+import glob
+
+from typing import Dict, Optional, Tuple, List, Union
 from collections import OrderedDict
 import argparse
 from torch.utils.data import DataLoader
 
 import flwr as fl
 import torch
+import numpy as np
 
 import utils
 
@@ -69,6 +73,60 @@ def get_evaluate_fn(model: torch.nn.Module, toy: bool):
     return evaluate
 
 
+net = utils.load_model()
+
+class CustomFedAvgStrategy(fl.server.strategy.FedAvg):
+    def aggregate_fit(
+        self,
+        server_round: int,
+        results: List[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes]],
+        failures: List[Union[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes], BaseException]],
+    ) -> Tuple[Optional[fl.common.Parameters], Dict[str, fl.common.Scalar]]:
+
+        # Call aggregate_fit from base class (FedAvg) to aggregate parameters and metrics
+        aggregated_parameters, aggregated_metrics = super().aggregate_fit(server_round, results, failures)
+
+        if aggregated_parameters is not None:
+            print(f"Saving round {server_round} aggregated_parameters...")
+
+            # Convert `Parameters` to `List[np.ndarray]`
+            aggregated_ndarrays: List[np.ndarray] = fl.common.parameters_to_ndarrays(aggregated_parameters)
+
+            # Convert `List[np.ndarray]` to PyTorch`state_dict`
+            params_dict = zip(net.state_dict().keys(), aggregated_ndarrays)
+            state_dict = OrderedDict({k: torch.tensor(v) for k, v in params_dict})
+            net.load_state_dict(state_dict, strict=True)
+
+            # Save the model
+            torch.save(net.state_dict(), f"ckpt/model_round_{server_round}.pth")
+
+        return aggregated_parameters, aggregated_metrics
+
+    def aggregate_evaluate(
+        self,
+        server_round: int,
+        results: List[Tuple[fl.server.client_proxy.ClientProxy, fl.common.EvaluateRes]],
+        failures: List[Union[Tuple[fl.server.client_proxy.ClientProxy, fl.common.FitRes], BaseException]],
+    ) -> Tuple[Optional[float], Dict[str, fl.common.Scalar]]:
+        """Aggregate evaluation accuracy using weighted average."""
+
+        if not results:
+            return None, {}
+
+        # Call aggregate_evaluate from base class (FedAvg) to aggregate loss and metrics
+        aggregated_loss, aggregated_metrics = super().aggregate_evaluate(server_round, results, failures)
+
+        # Weigh accuracy of each client by number of examples used
+        accuracies = [r.metrics["accuracy"] * r.num_examples for _, r in results]
+        examples = [r.num_examples for _, r in results]
+
+        # Aggregate and print custom metric
+        aggregated_accuracy = sum(accuracies) / sum(examples)
+        print(f"Round {server_round} accuracy aggregated from client results: {aggregated_accuracy}")
+
+        # Return aggregated loss and metrics (i.e., aggregated accuracy)
+        return aggregated_loss, {"accuracy": aggregated_accuracy}
+
 def main():
     """Load model for
     1. server-side parameter initialization
@@ -90,10 +148,18 @@ def main():
 
     model = utils.load_model()
 
+    # load pretrained weights
+    list_of_files = [fname for fname in glob.glob("ckpt/model_round_*")]
+    if len(list_of_files) > 0:
+        latest_round_file = max(list_of_files, key=os.path.getctime)
+        print("Loading pre-trained model from: ", latest_round_file)
+        state_dict = torch.load(latest_round_file)
+        model.load_state_dict(state_dict)
+
     model_parameters = [val.cpu().numpy() for _, val in model.state_dict().items()]
 
     # Create strategy
-    strategy = fl.server.strategy.FedAvg(
+    strategy = CustomFedAvgStrategy(
         fraction_fit=0.2,
         fraction_evaluate=0.2,
         min_fit_clients=5,
